496405e9667c170eac1ec1c5a9494f87dd0063fa
Dimassis <166415819+Dimassis@users.noreply.github.com>
2024-08-29 23:50:09 +0500

    Homework

--- untitled/src/main/java/org/example/Main.java
+++ untitled/src/main/java/org/example/Main.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            User.user("d1f","fdgf","fdgf");
+        } catch (WrongLoginException | WrongPasswordException e) {
+            System.out.println(e.getMessage());
+        }catch (Exception e) {
+            System.out.println("Ошибка " + e.getMessage());
+        } finally {
+            System.out.println("Проверка завершена");
+        }
     }
 }
